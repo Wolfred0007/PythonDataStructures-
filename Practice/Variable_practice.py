cbd0f10cd98499f6c7412f2fcc2ce1c97fa8b4c2
wolfred0007 <wilfredmainanjoroge@gmail.com>
2024-01-15 19:01:06 +0300

    practice

---
+++ Practice/Variable_practice.py
@@ -0,0 +1,20 @@
+Name="Wilfred"
+# print("my name is "+Name)
+age=36
+# print("my age is ",end="")
+# print(age,Name,sep="\n")
+
+
+# if age ==40:
+    # print("this is Wilfred")
+# else:
+    # print("that is not Wilfred")
+
+# Loops
+count=10
+
+
+for s in range(0,count):
+    print("I'm sorry")
+    if s ==5:
+        print("fifth iteration")
