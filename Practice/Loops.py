ad4a326c5143bcf56067c7394e8bde87d7767373
wolfred0007 <wilfredmainanjoroge@gmail.com>
2024-01-17 19:04:16 +0300

    Types of loops

---
+++ Practice/Loops.py
@@ -0,0 +1,18 @@
+numbers=[1,2,3,4,5]
+for num in numbers:
+    results=num+5
+    print(results,end=" ")
+names=["Onesumus,","Wilfred,","Carol","Veronica,"]
+for name in names : 
+    print(name,end=" ")
+    if name=="Carol":
+        print("This is our HR,")
+    else:
+        print("Team members")
+
+
+count = 0
+while count > 8:
+    print(count)
+    count =count+1
+
