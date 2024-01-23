b9af29f48bf4508ef8eed11c3b15ee3e46c0e8c9
wolfred0007 <wilfredmainanjoroge@gmail.com>
2024-01-23 18:10:43 +0300

    Email

---
+++ Practice/Email.py
@@ -0,0 +1,5 @@
+# name=("John","Wilfred","Onesmus")
+def send_email(Wilfred):
+    return "email sent to " + Wilfred
+results=send_email("Wambugu")
+print(results)
---
+++ Practice/Function_practice.py
@@ -0,0 +1,7 @@
+def sum_two_numbers(num1, num2):
+    results=num1+num2
+    return results
+
+print(sum_two_numbers(1,2))
+# results=sum_two_numbers(3,3)
+print(sum_two_numbers(3,4))
