f9c3af5d24a7a58b7f4e8ccc5068e4d976501bd8
wolfred0007 <wilfredmainanjoroge@gmail.com>
2024-01-24 18:46:14 +0300

    Variable practice

---
+++ Practice/Variable_types_practice.py
@@ -0,0 +1,11 @@
+age = 25
+price = 19.99
+name = "John"
+is_adult = True
+fruits = ["apple", "banana", "orange"]
+person = {"name": "Alice", "age": 30}
+coordinates = (5, 10)
+unique_numbers = {1, 2, 3, 4}
+print(fruits[2])
+print(person["name"])
+print(person["age"])
