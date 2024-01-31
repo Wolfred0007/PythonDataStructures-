75318c8c25c90fbd01425aeeb52e5d321b50e05c
wolfred0007 <wilfredmainanjoroge@gmail.com>
2024-01-31 19:10:46 +0300

    List

--- Practice/Variable_types_practice.py
+++ Practice/Variable_types_practice.py
@@ -6,6 +6,9 @@ fruits = ["apple", "banana", "orange"]
 person = {"name": "Alice", "age": 30}
 coordinates = (5, 10)
 unique_numbers = {1, 2, 3, 4}
+fruits[2]="grape"
 print(fruits[2])
 print(person["name"])
 print(person["age"])
+print(coordinates[1])
+coordinates[1]=0
