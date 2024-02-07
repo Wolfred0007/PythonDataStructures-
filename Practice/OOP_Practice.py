f4743e0d065c86757d15e944f547a4f7e338efbe
wolfred0007 <wilfredmainanjoroge@gmail.com>
2024-02-07 18:18:53 +0300

    Inherance in OOP

--- Practice/OOP_Practice.py
+++ Practice/OOP_Practice.py
@@ -13,9 +13,53 @@ print(dog2.name)
 my_dog.sound("bark")
 class Computer:
     def programs (self, Language, make, brand) :
-        make = Language
-        Language = brand 
-        print(Language)
+        
+        print(Language,brand,make)
 
 Computer1=Computer()
 Computer1.programs("Make","brand","Hp")
+
+
+class BankAccount:
+    def __init__(self, balance):
+        self._balance = balance  # Encapsulation using underscore
+
+    def get_balance(self):
+        return self._balance
+
+account = BankAccount(1000)
+print(account.get_balance())
+
+class Animal:
+    def speak(self):
+        pass
+
+class Dog(Animal):
+    def speak(self):
+        return "Woof!"
+
+class Cat(Animal):
+    def speak(self):
+        return "Purr!"
+
+my_dog = Dog()
+print(my_dog.speak())  # Outputs: Woof!
+my_cat= Cat()
+print(my_cat.speak())
+
+class Robot:
+    def move_left(self):
+        return "Moved Left"
+    def move_right(self):
+        return "Moved Right"
+    
+my_robot=Robot()
+print(my_robot.move_left())
+print(my_robot.move_right())
+
+class Minirobot(Robot):
+    pass
+my_minirobot=Minirobot() 
+print(my_minirobot.move_left())
+print(my_minirobot.move_right())
+# When inheritance takes place there's no need to repeat the def function.
